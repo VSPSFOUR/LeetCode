483e81d0abbe6d467435667c1145efb5afd1d227
Vyasa Aiyer <vyasa.aiyer@gmail.com>
2024-02-14 19:53:43 +0200

    Problem 2 - Attempt 2

--- Blind 75/Array/best_time_to_buy_and_sell_stock.py
+++ Blind 75/Array/best_time_to_buy_and_sell_stock.py
@@ -2,23 +2,44 @@
 
 from typing import List
 def maxProfit( prices: List[int]) -> int:
-    max_dif = 0
-    for i in range(len(prices)):
-        start_day = prices[i]
-        smallest_left_most = True
-        for j in range(1 + i , len(prices)):
-            end_day = prices[j]
-            if(start_day > end_day):
-                smallest_left_most = False
-            dif = end_day - start_day
-            if(dif > max_dif):
-                max_dif = dif 
-        if(smallest_left_most):
-            return max_dif
+#     max_dif = 0
+#     for i in range(len(prices)):
+#         start_day = prices[i]
+#         smallest_left_most = True
+#         for j in range(1 + i , len(prices)):
+#             end_day = prices[j]
+#             if(start_day > end_day):
+#                 smallest_left_most = False
+#             dif = end_day - start_day
+#             if(dif > max_dif):
+#                 max_dif = dif 
+#         if(smallest_left_most):
+#             return max_dif
         
-    return max_dif
+#     return max_dif
                 
         
-        
-test_value = maxProfit([7,1,5,3,6,4])
+    left_pointer = 0 
+    right_pointer = len(prices) -1 
+
+    left_min = prices[left_pointer]
+    right_max = prices[right_pointer]
+    
+    while(left_pointer <= right_pointer and left_pointer < len(prices) and right_pointer > 0):
+
+        left_value = prices[left_pointer]
+        right_value = prices[right_pointer]
+        if(left_value < left_min):
+            left_min = left_value
+        if(right_value > right_max):
+            right_max =right_value
+        left_pointer += 1
+        right_pointer -= 1
+    print("L :", left_min)
+    print("R : ", right_max)
+    differance = right_max  - left_min
+    if(differance < 0):
+        return 0
+    return right_max - left_min
+test_value = maxProfit([3,2,6,5,0,3])
 print(test_value)
